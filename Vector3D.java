2ead2fc9ef4e66f2f25bf03a628fe3847cfd34c4
Zhuo Fan Li <li.zhuofa@northeastern.edu>
2025-01-10 19:00:30 -0800

    finished questions 1 and 2

---
+++ Vector3D.java
@@ -0,0 +1,23 @@
+public class Vector3D {
+    double x;
+    double y;
+    double z;
+
+    public Vector3D(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    double getX() {
+        return x;
+    }
+
+    double getY() {
+        return y;
+    }
+
+    double getZ() {
+        return z;
+    }
+}
